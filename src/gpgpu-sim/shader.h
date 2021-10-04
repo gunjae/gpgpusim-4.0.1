58ac04731abeaf2ef0b40fbb7af05879dc92e1e9
Kiminje <sis013@naver.com>
2021-10-04 17:29:54 +0900

    point error check 3

--- src/gpgpu-sim/shader.h
+++ src/gpgpu-sim/shader.h
@@ -151,9 +151,9 @@ struct ldtime_stat {
 
 // IJ : data structure for cache status of mf
 struct pc_wrap {
-  ldtime_stat* pc_ldtime_stat;
+  ldtime_stat pc_ldtime_stat;
   enum cache_request_status status;
-  pc_wrap(ldtime_stat* i_stat, enum cache_request_status i_status) : pc_ldtime_stat(i_stat), status(i_status){};
+  pc_wrap(ldtime_stat i_stat, enum cache_request_status i_status) : pc_ldtime_stat(i_stat), status(i_status){};
 };
 
 // JH : data structure for analyzing ld_time
